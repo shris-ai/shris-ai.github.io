ebc1b41672416a230abd33b68b8de5ccd78b46e8
shristigautam13 <44741669+shristigautam13@users.noreply.github.com>
2025-02-11 05:18:32 +0530

    Initial commit

---
+++ src/routes/PaperDiveIns/papers.js
@@ -0,0 +1,38 @@
+// src/data/papers.js
+const papers = [
+    {
+        title: "Titans: Learning to Memorize at Test Time",
+        description:"This paper dives deep into the architecture and functioning of transformers, a key component in modern AI systems.",
+        tags: ["AI", "Machine Learning", "NLP"],
+        markdownFile: "", // Path to the Markdown file
+        htmlFile:"/src/routes/PaperDiveIns/papers/Titans_Learning to Memorize at Test Time.html",
+        img: {
+            src:"/src/routes/PaperDiveIns/papers/assets/Titans_Learning to Memorize at Test Time_0.png",
+            alt:"Research Paper"
+        }
+    },
+    {
+        title: "COCONUT: Training Large Language Models to Reason in a Continuous Latent Space",
+        description:"This paper dives deep into the architecture and functioning of transformers, a key component in modern AI systems.",
+        tags: ["AI", "Machine Learning", "NLP"],
+        markdownFile: "", // Path to the Markdown file
+        htmlFile:"/src/routes/PaperDiveIns/papers/COCONUT_Training Large Language Models to Reason in a Continuous Latent Space.html",
+        img: {
+            src:"/src/routes/PaperDiveIns/papers/assets/COCONUT_Training Large Language Models to Reason in a Continuous Latent Space_0.png",
+            alt:"COCONUT_Training Large Language Models to Reason in a Continuous Latent Space"
+        }
+    },
+    {
+        title: "DSPY: Compiling Declarative Language Model Calls into Self-Improving Pipelines",
+        description:"This paper dives deep into the architecture and functioning of transformers, a key component in modern AI systems.",
+        tags: ["AI", "Machine Learning", "NLP"],
+        markdownFile: "", // Path to the Markdown file
+        htmlFile:"/src/routes/PaperDiveIns/papers/DSPY_Compiling Declarative Language Model Calls into Self-Improving Pipelines.html",
+        img: {
+            src:"/src/routes/PaperDiveIns/papers/assets/DSPY_Compiling Declarative Language Model Calls into Self-Improving Pipelines_0.png",
+            alt:"DSPY_Compiling Declarative Language Model Calls into Self-Improving Pipelines"
+        }
+    }
+];
+
+export default papers;
