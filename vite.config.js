b25a81fbc8d75484f09f9dbd83790efe1c3089be
shristigautam13 <44741669+shristigautam13@users.noreply.github.com>
2025-02-11 07:00:49 +0530

    Fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    assetsInclude: ['**/*.html'], 
-    base: '/shris-ai.github.io/',
+    base: './', // Use relative path instead of /shris-ai.github.io/
     server: {
         host: true,
         open: true,
         watch: {
           usePolling: true,
         },
-      },
-      
+    },
+    build: {
+        outDir: "dist",
+        assetsDir: "assets",
+        emptyOutDir: true, // Clears the output directory before building
+        rollupOptions: {
+          input: "index.html", 
+        },
+    },
 });
