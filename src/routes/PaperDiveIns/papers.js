cba13bdb166a8a778c69bb9fc9c4bb00258472ec
shris-ai <44741669+shris-ai@users.noreply.github.com>
2025-02-12 21:08:46 +0530

    Paper Dive In: CoRAG

--- src/routes/PaperDiveIns/papers.js
+++ src/routes/PaperDiveIns/papers.js
@@ -1,6 +1,17 @@
 // src/data/papers.js
 const papers = [
     {
+        title: "CoRAG: Chain of Retrieval Augmented Generation",
+        description:"This paper introduces CoRAG, a dynamic retrieval framework that improves retrieval-augmented generation (RAG) by iteratively refining retrieval steps based on the reasoning process. Unlike traditional RAG models that use a fixed retrieval step, CoRAG trains LLMs to retrieve and reason step by step, leading to better multi-hop question answering. It achieves state-of-the-art performance on the KILT benchmark by optimizing retrieval strategies through rejection sampling, tree search, and best-of-N decoding. CoRAG enhances retrieval efficiency, reduces errors, and adapts dynamically to query complexity, making it a significant advancement in knowledge-intensive tasks.",
+        tags: ["AI", "Machine Learning", "NLP"],
+        markdownFile: "", // Path to the Markdown file
+        htmlFile:"/src/routes/PaperDiveIns/papers/CoRAG_Chain of Retrieval Augmented Generation.html",
+        img: {
+            src:"/src/routes/PaperDiveIns/papers/assets/CoRAG_Chain of Retrieval Augmented Generation_0.png",
+            alt:"Research Paper"
+        }
+    },
+    {
         title: "Titans: Learning to Memorize at Test Time",
         description:"This paper dives deep into the architecture and functioning of transformers, a key component in modern AI systems.",
         tags: ["AI", "Machine Learning", "NLP"],
